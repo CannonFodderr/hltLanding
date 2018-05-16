1c3e9e44024e235e67a32670c1f3d846c5294077
CannonFodderr <izhaki.idan@gmail.com>
2018-05-16 17:24:32 +0300

    Langing page with Active Trail form

---
+++ script.js
@@ -0,0 +1,38 @@
+
+        var errMsg = "כתובת דואר אלקטרוני חסרה או שגויה";
+        function Validate() {
+            var emailFilter = /^.+@.+\..{2,3}$/;
+            var txtMail = document.getElementById("mm_newemail");
+            if (txtMail == null) {
+                return false;
+            }
+            else if (!(emailFilter.test(txtMail.value))) {
+                showErr(txtMail);
+                txtMail.style.backgroundColor = "#ffcccc";
+                txtMail.focus();
+                return false;
+            }
+            else {
+                txtMail.style.backgroundColor = "white";
+                removeErr();
+                return true;
+            }
+        }
+        function removeErr() {
+            var errSpan = document.getElementById("errph");
+            if (errSpan != null) {
+                errSpan.parentNode.removeChild(errSpan);
+            }
+        }
+        function showErr(txtMail) {
+            if (document.getElementById("errph") == null) {
+                var errDiv = document.createElement('DIV');
+                var innerDiv = document.createElement('DIV');
+                errDiv.id = "errph";
+                errDiv.className = "errph";
+                innerDiv.innerHTML = errMsg;
+                innerDiv.className = "errinnerph";
+                errDiv.appendChild(innerDiv);
+                txtMail.parentNode.appendChild(errDiv);
+            }
+        }
