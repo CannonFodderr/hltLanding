87e080ac0d146b4c6e80e39cb774a9346b6ac756
CannonFodderr <izhaki.idan@gmail.com>
2018-05-17 15:10:28 +0300

    Add clock countdown

---
+++ func.js
@@ -0,0 +1,18 @@
+var endTime = new Date("May 31, 2018 23:37:25").getTime();  
+
+var x = setInterval(function (){   
+    var thisTime = new Date().getTime();   
+    var diff = endTime - thisTime;
+
+    var days = Math.floor(diff / (1000*60*60*24));
+    var hours = Math.floor(diff % (1000*60*60*24) / (1000 * 60 * 60));
+    var minutes = Math.floor(diff % (1000*60*60) / (1000 * 60));
+    var seconds = Math.floor(diff % (1000*60) / 1000);
+    document.getElementById("clock").innerHTML = days + " יום " + hours + " שעות "
+  + minutes + " דקות " + "<span class='highlight'>" + seconds + "</span>" + " שניות ";
+
+  if (diff < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "המבצע הסתיים";
+  }
+}, 1000)
